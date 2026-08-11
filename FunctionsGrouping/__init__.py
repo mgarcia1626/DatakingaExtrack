6be4e9d927826755cd76b5ac3b875e54957562be
mgarcia1626 <matiasegarcia97@gmail.com>
2026-08-11 20:02:18 -0300

    fix: clear __init__.py to avoid importing bs4/requests on dashboard startup

--- FunctionsGrouping/__init__.py
+++ FunctionsGrouping/__init__.py
@@ -1,7 +1,1 @@
-"""
-FunctionsGrouping - Agrupación de funciones del proyecto.
-"""
-
-from .auth_functions import login, get_page_html, click_cinta_testigo, click_procesar, exportar_excel
-
-__all__ = ['login', 'get_page_html', 'click_cinta_testigo', 'click_procesar', 'exportar_excel']
+# FunctionsGrouping package
